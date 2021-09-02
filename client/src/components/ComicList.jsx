76a8ca5335b28f3adbb00cbc4a151d80959719fb
MistermindX <max.arenstein@gmail.com>
2021-09-02 15:10:23 -0400

    fixed useEffect

--- client/src/components/ComicList.jsx
+++ client/src/components/ComicList.jsx
@@ -5,18 +5,23 @@ import axios from 'axios'
 function ComicList(seller) {
   const [comicListsBySeller, setComicListsBySeller] = useState([])
 
+  useEffect(() => {
+    return () => {
+      getAllUserComics()
+    }
+  }, [])
+
   const getAllUserComics = async (e) => {
     try {
       const res = await axios.get(`${BASE_URL}/comics/seller/${seller._id}`)
-      setComicListsBySeller(...res.data)
+      console.log(res.data)
+      setComicListsBySeller(res.data)
     } catch (err) {
       console.log(err)
+    } finally {
+      console.log(comicListsBySeller)
     }
   }
-
-  useEffect(() => {
-    getAllUserComics()
-  }, [])
 
   return (
     <ul>
@@ -24,7 +29,7 @@ function ComicList(seller) {
         <li key={comic._id}>
           <h3>{comic.title}</h3>
           <h5>{comic.grade}</h5>
-          <h5>{comic.price}</h5>
+          <h5>${comic.price}</h5>
         </li>
       ))}
     </ul>
