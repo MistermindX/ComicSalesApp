b35582bf0e85e5cb81d67ef510bfa9e7388f571e
MistermindX <max.arenstein@gmail.com>
2021-09-02 12:52:29 -0400

    wrote map function

--- client/src/components/ComicList.jsx
+++ client/src/components/ComicList.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BASE_URL } from '../globals'
 import axios from 'axios'
 
 function ComicList(seller) {
+  const [comicListsBySeller, setComicListsBySeller] = useState([])
+
   const getAllUserComics = async (e) => {
     try {
       const res = await axios.get(`${BASE_URL}/comics/seller/${seller._id}`)
-      console.log(res.data)
+      setComicListsBySeller(...res.data)
     } catch (err) {
       console.log(err)
     }
@@ -16,7 +18,17 @@ function ComicList(seller) {
     getAllUserComics()
   }, [])
 
-  return <div>test</div>
+  return (
+    <ul>
+      {comicListsBySeller.map((comic) => (
+        <li key={comic._id}>
+          <h3>{comic.title}</h3>
+          <h5>{comic.grade}</h5>
+          <h5>{comic.price}</h5>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default ComicList
