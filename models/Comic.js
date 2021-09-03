e1b521b977ea0fae1a96aeff9215b73ee5a294dc
MistermindX <max.arenstein@gmail.com>
2021-09-03 11:12:16 -0400

    made images optional

--- models/Comic.js
+++ models/Comic.js
@@ -4,7 +4,7 @@ const Comic = new Schema(
   {
     title: { type: String, required: true },
     grade: { type: String, required: true },
-    img: { type: String, required: true },
+    img: { type: String },
     price: { type: Number, required: true },
     seller_id: { type: Schema.Types.ObjectId, ref: 'seller_id' }
   },
